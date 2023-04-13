b0c719d04f0c68bfc5944f3a8e62fd36ac33b95a
yaochengkuo <115142539+yaochengkuo@users.noreply.github.com>
2023-04-13 16:05:41 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,20 @@
-
+input.onGesture(Gesture.Shake, function () {
+    拍手次數 = -1
+})
+input.onSound(DetectedSound.Loud, function () {
+    if (input.soundLevel() > 110) {
+        拍手次數 += 1
+        basic.showLeds(`
+            . . . . #
+            . # . . #
+            # # # . #
+            . # . . #
+            . . . . #
+            `)
+        basic.showNumber(拍手次數)
+    }
+})
+let 拍手次數 = 0
+input.setSoundThreshold(SoundThreshold.Loud, 110)
+拍手次數 = 0
+let 減去 = 0
